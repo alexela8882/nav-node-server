7b7e2880cb17e1b3ad629c50713e42bb285f8f77
alexela8882 <alexela8882@gmail.com>
2022-07-04 08:35:32 +0800

    whitelisted origin domains added for cors security

--- index.js
+++ index.js
@@ -4,17 +4,35 @@ var httpntlm = require('httpntlm');
 var bodyParser = require("body-parser");
 var router = express.Router();
 
-var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+var app = express()
+
+var whitelist = [
+  'http://localhost:8000',
+  'http://uat.reddotcrm.com',
+  'https://uat.reddotcrm.com',
+  'https://reddotcrm.com'
+]
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 
 var PORT = 4000;
 
 router.get('/', function(req, res) {
     // res.status(200).send('Hello world');
 
-    console.log("Fetching data. Please wait...");
+    console.log("Fetching data. Please wait...")
 
     httpntlm.get({
       url: "http://LEKZPC:7048/DynamicsNAV100/ODataV4/Company('CRONUS%20International%20Ltd.')",
